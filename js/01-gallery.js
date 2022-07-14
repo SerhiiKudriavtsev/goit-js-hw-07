457bf67314e9cce57ca3b13086299abd0d925cfc
Serhii Kudriavtsev <96264045+SerhiiKudriavtsev@users.noreply.github.com>
2022-07-14 09:01:01 +0300

    Update - 03
    Update - 03

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -20,28 +20,59 @@ const items = galleryItems.map(({ preview, original, description }) => {
 </div>
   `}).join('');
   
-  gallery.insertAdjacentHTML("afterbegin", items);
-  
-function keyDown(event) {
-    if (event.key === 'Escape') {
-          openInstance.close();
-        }  
-};
+gallery.insertAdjacentHTML("afterbegin", items);
 
-let openInstance;
-const instance = document.querySelector('.gallery').onclick = (event) => {
+// ========= V-last =====
+  
+gallery.addEventListener('click', onImgClick);
+
+const instance = basicLightbox.create(`<img src="">`,
+  {
+    onShow: instance => {
+      window.addEventListener('keydown', keyDown);
+    },
+    onClose: instance => {
+      window.addEventListener('keydown', keyDown);
+    }
+  });
+
+function onImgClick(event) {
+  event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-  basicLightbox.create(`
-      <img src="${event.target.dataset.source}">
-    `, {
-    onShow: (instance) => {        
-      window.addEventListener('keydown', keyDown);
-      openInstance = instance;
-    },
-    onClose: () => {
-      window.removeEventListener;
-    }
-  }).show();
+  instance.element().querySelector('img').src = event.target.dataset.source;
+  instance.show();
 }
+
+function keyDown(event) {
+  if (event.key === 'Escape') { 
+    instance.close();
+    return;
+  }
+}
+
+  // ==== V-1 ===== 
+//   let openInstance;
+// gallery.onclick = (event) => { //взяв з прикладу в документації бібліотеки
+//   event.preventDefault();
+//     if (!event.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+//     basicLightbox.create(`<img src="${event.target.dataset.source}">`,
+//       {
+//       onShow: instance => {        
+//         window.addEventListener('keydown', keyDown);
+//         openInstance = instance;
+//       },
+//       onClose: instance => {
+//         window.removeEventListener('keydown', keyDown);
+//       }
+//   }).show();
+//   }
+
+// function keyDown(event) {
+//     if (event.key === 'Escape') {
+//       openInstance.close();
+//     }  
+// };
